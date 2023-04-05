fe8e240727e5128267869fde8422097c1a097a64
Vaibhav <kasarvaibhav777@gmail.com>
2023-04-05 23:16:05 +0530

    completed homepage of website

--- src/app.js
+++ src/app.js
@@ -1,16 +1,64 @@
 const { response } = require("express")
 const express = require("express")
-const hbs = require("hbs")
 const app= express()
+
+const hbs = require("hbs")
+
+const mongoose=require("mongoose")
 const routes=require("./routes/main")
+const Detail=require("./models/Detail")
+const Slider=require("./models/Slider")
+
 
 app.use('',routes)
 
+// /static/css/style.css
+app.use('/static',express.static("public"))
+
+
+
 //(template engine)
-app.use(express.static("public"))
 app.set('view engine','hbs')
 app.set("views","views")
+hbs.registerPartials("views/partials") 
 
+
+//connecting to database
+mongoose.connect("mongodb://127.0.0.1:27017/dynamic_web",{
+    useNewUrlParser: true
+})
+.then( ()=> console.log("MongoDb is connected"))
+.catch( err => console.log(err))
+
+// Detail.create({
+//     brandName:"VK Brother's Tech Solutions Pvt.Ltd",
+//     brandIconUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB4OSb6JUpl0DMynkb0iqVYWbgMn7V7mL2Gw&usqp=CAU",
+//     links:[
+//         {
+//             label:"Home",
+//             url:"/"
+//         },
+//         {
+//             label:"Services",
+//             url:"/services"
+//         },
+//         {
+//             label:"Gallery",
+//             url:"/gallery"
+//         },
+//         {
+//             label:"About",
+//             url:"/about"
+//         },
+//         {
+//             label:"Contact Us",
+//             url:"/contact-us"
+//         },
+//     ]
+       
+// })
+
+//localhost server
 app.listen(process.env.PORT | 5556,()=>{
     console.log("Server Started...")
 })
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,15 +1,20 @@
-const express=require("express")
+const express = require("express");
+const routes = express.Router();
 
+const Detail = require("../models/Detail");
+const Slider = require("../models/Slider");
 
-const routes=express.Router()
+routes.get("/", async (req, res) => {
+  const details = await Detail.findOne({ _id: "642a8cc1d55c26264ee5eded" });
+  const slides = await Slider.find();
+  //console.log(details)
+  //console.log(slides)
+  res.render("index", { details: details, slides:slides });
+});
 
-routes.get("/",(req,res)=>{
-    res.render("Index")
-})
+routes.get("/gallery", async (req, res) => {
+  const details = await Detail.findOne({ _id: "642a8cc1d55c26264ee5eded" });
+  res.render("gallery", { details: details });
+});
 
-routes.get("/gallery",(req,res)=>{
-    res.render("gallery")
-})
-
-
-module.exports=routes
+module.exports = routes;
